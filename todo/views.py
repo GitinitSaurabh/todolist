36639b58a2906dc755056f81cde42e5886916133
GitinitSaurabh <saurabh160196@gmail.com>
2021-02-08 17:54:50 +0530

    Created signup function for users

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login
+def signupuser(request):
+    if request.method == 'GET':
+        return render(request, 'todo/signupuser.html',{'form':UserCreationForm()})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('current')
 
-# Create your views here.
+            except IntegrityError:
+                return render(request, 'todo/signupuser.html',{'form':UserCreationForm(),'error':'User Already Exists!'})
+
+        else:
+            return render(request, 'todo/signupuser.html',{'form':UserCreationForm(),'error':'Passwords do not match!'})
+
+
+def current(request):
+    return render(request, 'todo/current.html')
